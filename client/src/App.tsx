41f46af6528c35178a425c7aa69a1aefb82dba96
Shakana Jamal <shakana.jamal.moalin@iths.se>
2022-06-26 14:00:36 +0200

    added api request in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import "./App.css";
+import { useState, useEffect } from "react";
+import * as api from "./api/index";
+import { QuizType } from "./interface/quizType";
 
 const App = () => {
-  return <>
-  <h1>Hello from APP</h1>
-  </>;
+  const [allQuizes, setQuiz] = useState<QuizType[]>([]);
+
+  useEffect(() => {
+    const loadQuizes = async () => {
+      const res = await api.getAllQuizes();
+      setQuiz(res.data);
+    };
+    loadQuizes();
+  }, []);
+  return (
+    <>
+      <h1>Hello from APP</h1>
+      {allQuizes &&
+        allQuizes.map((quiz, index: number) => (
+          <li key={index}>{quiz.titel}</li>
+        ))}
+    </>
+  );
 };
 
 export default App;
---
+++ client/src/interface/quizType.ts
@@ -0,0 +1,10 @@
+export interface QuestionType {
+  term: string;
+  definition: string;
+}
+
+export interface QuizType {
+  titel: string;
+  description: string;
+  questions: Array<QuestionType>;
+}
